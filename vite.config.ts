5fba6e352905b36c02ffc4f67c8a8ea2bb25957a
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-06-19 21:11:55 +0300

    chore:🌻 added some info in gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,30 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.github ? 'landing-state-vex' : '',
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+// export default defineConfig({
+//   base: process.env.GITHUB ? process.env.BASE_URL : '',
+//   plugins: [vue(), vueJsx()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+export default ({ mode }) => {
+  process.env = {
+    ...process.env,
+    ...loadEnv(mode, process.cwd())
   }
-})
+  return defineConfig({
+    // base : ? 'landing-state-vex' : '',
+    base: process.env.VITE_API_KEY !== 'false' ? 'landing-state-vex' : '',
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    }
+  })
+}
