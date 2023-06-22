d4dcaac89910b5a52aabd7ff137a5468dc32ce62
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-06-22 18:29:30 +0300

    🎯 style: added some logic

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -12,10 +12,12 @@ const navigateTo = () => {
 </script>
 
 <template>
-  <div class="header-container">
-    <div class="header-container__search" @click="ate.decrement()">TES</div>
-    <div>{{ ate.count }}, Double {{ double }}</div>
-    <div @click="ate.increment()">Items</div>
+  <div class="header">
+    <div class="header-container">
+      <div class="header-container__search" @click="ate.decrement()">TES</div>
+      <div>{{ ate.count }}, Double {{ double }}</div>
+      <div @click="ate.increment()">Items</div>
+    </div>
     <div class="header-container__action">
       <button @click="navigateTo">Log out</button>
     </div>
@@ -23,27 +25,36 @@ const navigateTo = () => {
 </template>
 
 <style scoped lang="scss">
-.header-container {
-  padding: 0 4rem 0 2rem;
-  height: 100%;
+.header {
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-between;
   align-items: center;
+  height: 100%;
 
-  &__action {
-    button {
-      height: 2rem;
-      background: var(--color-sidebar-back);
-      border: 1px solid var(--color-active-link);
-      border-radius: 0.8rem;
-      color: var(--color-text);
-      font-size: 0.9rem;
-      font-weight: 500;
+  &-container {
+    padding: 0 4rem 0 2rem;
+    height: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
 
-      &:hover {
-        background: var(--color-primary-hover);
-        font-weight: 700;
-        color: #fdfffe;
+    &__action {
+      padding-right: 2rem;
+
+      button {
+        height: 2rem;
+        background: var(--color-sidebar-back);
+        border: 1px solid var(--color-active-link);
+        border-radius: 0.8rem;
+        color: var(--color-text);
+        font-size: 0.9rem;
+        font-weight: 500;
+
+        &:hover {
+          background: var(--color-primary-hover);
+          font-weight: 700;
+          color: #fdfffe;
+        }
       }
     }
   }
