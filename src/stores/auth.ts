679782ce356d324eeecdfaf7982471bfcd387fd1
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-06-22 10:46:54 +0300

    chore:🌻 remove empty line

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -2,21 +2,17 @@ import { defineStore } from 'pinia'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { auth } from '../../firebase/firebase'
 //import auht from firebase
-export const useAuthStore = defineStore(
-  'auth',
-
-  {
-    state: () => ({}),
-    getters: {},
-    actions: {
-      registerWithEmail(credentaial: { email: string; password: string }) {
-        createUserWithEmailAndPassword(auth, credentaial.email, credentaial.password)
-          .then((value) => {
-            console.log(value)
-          })
-          .catch((err) => console.log(err))
-      }
-      //register with firebase create user with name and password
+export const useAuthStore = defineStore('auth', {
+  state: () => ({}),
+  getters: {},
+  actions: {
+    registerWithEmail(credentaial: { email: string; password: string }) {
+      createUserWithEmailAndPassword(auth, credentaial.email, credentaial.password)
+        .then((value) => {
+          console.log(value)
+        })
+        .catch((err) => console.log(err))
     }
+    //register with firebase create user with name and password
   }
-)
+})
