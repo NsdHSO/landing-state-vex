4f43c633dfb0c5a9085a913a2d92cfd34a92c325
NsdHSO <37635083+NsdHSO@users.noreply.github.com>
2023-07-06 04:37:37 +0000

    Deploying to gh-pages from  @ aade3d1d8874a03d7928600e122875d73c0ef056 🚀

---
+++ assets/PumpView-7845b6fc.js
@@ -0,0 +1,68 @@
+import{_ as b,o as i,e as d,j as L,f as e,F as w,i as z,t as M,a as k,k as v,l as A,c as E,h as r,w as I,G,D as N,m as K,n as Y,p as j,q,s as T}from"./index-b44f18cd.js";const H={},J={version:"1.0",xmlns:"http://www.w3.org/2000/svg",width:"144.000000pt",height:"136.000000pt",viewBox:"0 0 144.000000 136.000000",preserveAspectRatio:"xMidYMid meet"},O=L(`<g transform="translate(0.000000,136.000000) scale(0.100000,-0.100000)" stroke="none"><path d="M648 1329 c-21 -10 -44 -33 -53 -49 -17 -34 -20 -108 -4 -130 9 -13
+10 -12 5 3 -3 11 0 17 9 17 9 0 15 10 15 25 0 15 7 39 15 55 47 91 185 50 185
+-54 0 -16 6 -26 15 -26 9 0 12 -6 9 -17 -5 -15 -4 -16 5 -3 16 22 13 96 -4
+130 -18 36 -82 70 -127 70 -18 -1 -50 -10 -70 -21z"></path><path d="M100 673 c24 -10 51 -13 84 -9 39 4 54 1 76 -14 17 -12 44 -20 67
+-20 l38 1 -35 29 c-34 30 -36 30 -150 29 -112 -1 -114 -1 -80 -16z"></path><path d="M440 660 l35 -29 245 0 245 0 35 29 35 29 -315 0 -315 0 35 -29z"></path><path d="M1110 660 l-35 -29 40 0 c24 0 50 7 65 19 20 16 35 18 81 13 40 -4
+63 -1 85 10 28 16 25 16 -86 17 -114 0 -116 0 -150 -30z"></path><path d="M97 609 c-47 -27 -63 -135 -27 -181 10 -14 33 -31 50 -38 38 -16 48
+-51 26 -86 -33 -49 -68 -41 -83 19 -7 28 -8 24 -13 -33 -6 -76 -3 -89 16 -62
+7 11 27 22 43 25 20 4 39 19 55 44 51 77 43 136 -23 177 -54 35 -67 60 -46 92
+8 13 20 24 25 24 14 0 44 -36 52 -63 4 -15 6 -6 7 26 1 29 -3 46 -9 42 -5 -3
+-20 1 -32 9 -16 11 -28 12 -41 5z"></path><path d="M260 600 c-25 -4 -39 -8 -32 -8 17 -3 17 -243 0 -260 -18 -18 -4 -15
+47 10 25 12 41 24 36 27 -12 8 -17 83 -7 99 4 7 20 12 37 12 27 0 29 -3 29
+-37 0 -21 -8 -45 -17 -56 -17 -19 -17 -19 7 -12 42 13 86 33 73 34 -16 1 -18
+172 -3 187 6 6 -6 9 -35 9 -43 0 -56 -7 -35 -20 12 -7 14 -72 3 -78 -5 -2 -20
+-7 -35 -11 -28 -6 -28 -6 -28 42 0 26 5 53 12 60 7 7 8 11 3 10 -6 0 -30 -4
+-55 -8z"></path><path d="M473 599 c14 -8 17 -24 17 -89 0 -65 -3 -81 -17 -89 -23 -14 -22 -13
+76 4 82 14 101 24 101 54 0 11 -6 9 -22 -7 -13 -11 -36 -22 -53 -24 -27 -3
+-30 0 -33 30 -4 36 19 45 37 15 8 -14 10 -8 10 27 0 35 -2 41 -10 28 -16 -29
+-39 -22 -39 12 0 28 3 30 35 30 22 0 37 -6 44 -17 12 -23 22 -13 14 15 -5 21
+-11 22 -92 22 -63 -1 -81 -3 -68 -11z"></path><path d="M688 599 c16 -17 16 -131 0 -147 -9 -9 -1 -12 37 -12 38 0 46 3 37
+12 -14 14 -16 58 -3 58 5 0 24 -18 41 -40 24 -31 39 -40 62 -40 l30 0 -37 42
+c-35 41 -35 43 -15 52 24 11 27 48 5 66 -9 7 -50 15 -93 18 -63 3 -75 2 -64
+-9z m117 -39 c0 -20 -6 -26 -27 -28 -25 -3 -28 0 -28 28 0 28 3 31 28 28 21
+-2 27 -8 27 -28z"></path><path d="M914 594 c20 -19 23 -149 4 -160 -8 -5 1 -12 23 -20 47 -17 67 -18
+51 -2 -16 16 -16 170 0 186 9 9 0 12 -41 12 -48 0 -51 -1 -37 -16z"></path><path d="M1030 598 c0 -5 5 -8 10 -8 6 0 10 -38 10 -95 0 -83 -2 -95 -17 -95
+-23 0 -6 -15 32 -28 17 -5 39 -13 50 -18 19 -7 19 -7 3 13 -18 19 -26 103 -10
+103 13 0 27 -15 39 -40 10 -22 12 -17 12 40 1 61 0 63 -15 42 -27 -39 -44 -28
+-44 28 0 45 2 50 23 50 31 0 48 -13 63 -47 l12 -28 1 42 1 42 -85 3 c-47 2
+-85 0 -85 -4z"></path><path d="M1228 588 c8 -8 12 -54 12 -139 0 -114 -2 -128 -17 -131 -12 -2 -1
+-13 37 -35 30 -17 56 -30 58 -28 2 2 -3 14 -12 27 -17 24 -23 138 -7 138 14 0
+28 -22 39 -60 9 -30 10 -22 11 55 0 87 0 89 -14 57 -23 -52 -40 -35 -43 41 -3
+65 -2 67 21 67 33 0 54 -19 65 -60 10 -34 10 -33 11 23 l1 57 -87 0 c-68 0
+-84 -3 -75 -12z"></path><path d="M553 388 c-49 -9 -53 -12 -62 -46 -6 -20 -9 -37 -8 -38 2 -3 135 81
+145 91 7 6 -11 5 -75 -7z"></path><path d="M813 394 c11 -12 141 -93 144 -90 1 1 -2 18 -7 38 -9 31 -15 37 -48
+42 -20 4 -50 9 -67 13 -16 3 -26 2 -22 -3z"></path><path d="M590 290 l-125 -80 0 -46 c-1 -39 -6 -51 -33 -75 -43 -39 -84 -38
+-123 0 -56 56 -27 145 50 157 36 6 39 9 49 53 6 25 9 47 7 49 -8 8 -55 -12
+-55 -24 0 -7 -21 -24 -48 -36 -62 -31 -98 -96 -87 -158 13 -77 106 -140 177
+-120 69 20 118 77 118 137 0 30 7 38 93 93 50 33 99 60 107 60 8 0 57 -27 108
+-60 85 -55 92 -63 92 -93 0 -92 103 -162 195 -130 128 43 136 210 13 271 -27
+12 -48 29 -48 36 0 12 -47 32 -55 24 -2 -2 1 -24 7 -49 10 -44 13 -47 49 -53
+106 -17 104 -166 -3 -183 -33 -5 -41 -2 -74 31 -33 33 -36 40 -30 75 l6 39
+-129 81 c-70 44 -130 81 -132 81 -2 0 -60 -36 -129 -80z"></path></g>`,1),Q=[O];function U(o,c){return i(),d("svg",J,Q)}const W=b(H,[["render",U]]),X={},Z={version:"1.0",xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 1280.000000 791.000000",preserveAspectRatio:"xMidYMid meet"},e1=e("metadata",null," Created by potrace 1.15, written by Peter Selinger 2001-2017 ",-1),n1=e("g",{transform:"translate(0.000000,791.000000) scale(0.100000,-0.100000)",stroke:"none"},[e("path",{d:`M5505 7322 c-38 -21 -284 -160 -545 -309 -261 -149 -686 -391 -945
+-538 -258 -147 -495 -285 -525 -308 -178 -133 -408 -406 -491 -580 -46 -97
+-36 -186 37 -352 20 -44 41 -92 46 -106 11 -24 -43 -91 -1066 -1318 -592 -711
+-1079 -1298 -1082 -1306 -3 -7 -1 -23 5 -34 10 -18 0 -33 -93 -133 l-104 -113
+-51 3 c-73 5 -125 -24 -238 -132 -87 -82 -221 -248 -286 -352 -25 -41 -26 -43
+-11 -103 74 -318 382 -621 709 -700 152 -36 131 -41 252 58 245 201 385 383
+369 481 -3 21 -8 45 -12 53 -3 9 37 59 108 136 111 121 113 122 149 116 22 -4
+40 -2 43 4 4 6 89 -51 204 -137 641 -482 1286 -934 1680 -1177 141 -87 143
+-88 181 -77 134 41 524 440 1107 1132 382 453 803 977 1047 1300 19 25 76 125
+127 223 l92 178 160 157 c358 352 586 647 650 842 17 55 22 101 28 305 7 234
+8 247 58 505 29 146 54 274 56 285 3 11 39 45 81 74 144 103 225 207 225 289
+0 12 5 22 11 22 6 0 105 57 221 126 224 135 216 127 192 187 -4 12 10 20 63
+36 665 195 1302 341 1753 401 249 33 441 43 602 30 378 -30 929 -157 1603
+-369 182 -57 191 -59 215 -44 53 35 121 117 154 186 46 96 60 176 53 290 -7
+110 -13 134 -35 140 -10 2 -118 36 -242 75 -708 223 -1308 354 -1685 369 -489
+18 -1399 -122 -2459 -379 -198 -48 -210 -50 -222 -33 -14 19 -66 19 -359 -1
+l-90 -6 -18 38 c-52 111 -97 143 -212 151 -128 10 -327 -32 -530 -112 l-88
+-34 -96 44 c-87 40 -100 50 -144 107 -203 265 -425 448 -543 448 -26 0 -63
+-13 -109 -38z m-980 -3027 c154 -113 287 -213 297 -223 15 -16 172 -372 166
+-377 -2 -1 -95 7 -208 17 -113 10 -227 17 -255 14 -60 -5 -192 -59 -261 -106
+-95 -66 -189 -166 -252 -272 -24 -38 -44 -96 -384 -1108 -115 -344 -211 -626
+-212 -628 -3 -2 -804 1049 -826 1085 -12 18 52 94 714 861 l727 841 97 50 c53
+28 102 50 107 50 6 1 136 -92 290 -204z m293 -1111 l240 -86 -15 -44 c-8 -24
+-16 -44 -18 -44 -2 0 -28 10 -59 21 -31 12 -59 19 -62 16 -4 -3 -35 -155 -70
+-337 -34 -183 -65 -338 -68 -346 -5 -12 -563 -732 -590 -761 -12 -13 -412 -70
+-422 -61 -3 3 112 361 256 796 292 883 274 843 392 901 101 51 134 46 416 -55z`}),e("path",{d:`M12122 6058 c-40 -50 -131 -282 -167 -427 -93 -368 -22 -567 200
+-565 99 0 169 44 211 132 23 48 31 211 14 307 -26 148 -121 395 -199 518 -32
+49 -42 55 -59 35z`})],-1),t1=[e1,n1];function l1(o,c){return i(),d("svg",Z,t1)}const s1=b(X,[["render",l1]]);const o1={},a1={class:"wrapper-calc"},c1=["onClick"];function i1(o,c){return i(),d("div",null,[e("div",a1,[(i(!0),d(w,null,z(Array.from({length:13}).map((l,a)=>{if(a>=1)return a===10?null:a===11?0:a===12?"Clear":a}).filter(l=>l!=null),(l,a)=>(i(),d("div",{class:"wrapper-calc__key",key:a,onClick:y=>o.$emit("keyPress",{item:l})},M(l),9,c1))),128))])])}const d1=b(o1,[["render",i1],["__scopeId","data-v-491de01b"]]),_1=["disabled","value"],r1={class:"show-value__label"},u1={for:""},p1=k({__name:"ShowValueComponent",props:{label:String,model:Number,disabledModel:Boolean},setup(o){return(c,l)=>(i(),d("div",null,[e("input",{type:"number",disabled:o.disabledModel,value:o.model},null,8,_1),e("div",r1,[e("label",u1,M(o.label),1)])]))}});const g=b(p1,[["__scopeId","data-v-99b747ea"]]),h=o=>(j("data-v-a10d0194"),o=o(),q(),o),v1={class:"pump"},h1={class:"pump__top"},m1={class:"pump__top-preset"},f1={class:"pump__bottom"},M1={class:"pump__bottom-coins"},b1={class:"pump__bottom__fuel"},g1={class:"pump__bottom__fuel-price"},w1={class:"pump__bottom__fuel-pump"},y1=["onMousedown"],$1={class:"generic-invoice"},z1={class:"generic-invoice__price"},C1=h(()=>e("span",null," Price: ",-1)),I1={class:"generic-invoice__price-money"},k1=h(()=>e("div",{class:"generic-invoice__price-label"},[e("span",null,"Dollars")],-1)),x1={class:"generic-invoice__price"},P1=h(()=>e("span",null," Litters: ",-1)),S1={class:"generic-invoice__price-money"},D1=h(()=>e("div",{class:"generic-invoice__price-label"},[e("span",null,"Litter")],-1)),R1={class:"generic-invoice__price liter"},V1=h(()=>e("span",null," Price per liter: ",-1)),B1={class:"generic-invoice__price-money"},F1=h(()=>e("div",{class:"generic-invoice__price-label"},[e("span",null,"Litter")],-1)),L1=k({__name:"PumpView",setup(o){const c=v(0),l=v(0),a=v(0),y=v(0),$=v(!1),u=A([{valueModel:0,label:"Fuel 1",fuel:1},{valueModel:0,label:"Fuel 2",fuel:2},{valueModel:0,label:"Fuel 3",fuel:3}]),x=E(()=>u?u.find(n=>n.valueModel>0):0),P=v(!0),C=[{class:"green-one",name:"Green",fuel:0,price:3.2},{class:"blue-two",name:"Blue",fuel:1,price:2.2},{class:"red-three",name:"Red",fuel:2,price:1.2}];function S(n){n.item!=="Clear"?(a.value=+(a.value+""+n.item),c.value=0,l.value=0):a.value=0}function D(n,t,s,_,p){for(let m=0;m<=n;m++)setTimeout(()=>{t+=s,u[_.fuel].valueModel=+t.toFixed(3)},m*p)}function R(n){const t=n.price/100,s=300,_=100,p=s/_;let m=0;c.value=0,l.value=0,u.forEach(f=>f.valueModel>0?f.valueModel=0:f.valueModel),D(_,m,t,n,p),V(p),T(c,f=>{l.value=+(f/n.price).toFixed(3)})}function V(n){y.value=setInterval(()=>{c.value++},n)}function B(){clearInterval(y.value),$.value=!0}function F(n){n==="closeModal"&&($.value=!1,c.value=0,l.value=0,u.forEach(t=>t.valueModel>0?t.valueModel=0:t.valueModel))}return(n,t)=>(i(),d(w,null,[e("div",v1,[e("div",h1,[e("div",m1,[e("div",null,[r(W),r(g,{model:a.value,label:"Preset","disabled-model":P.value},null,8,["model","disabled-model"])]),e("div",null,[r(d1,{onKeyPress:t[0]||(t[0]=s=>S(s))})])])]),e("div",f1,[e("div",M1,[r(g,{model:c.value,label:"Dollar"},null,8,["model"]),r(g,{model:l.value,label:"Liters"},null,8,["model"])]),e("div",b1,[e("div",g1,[(i(!0),d(w,null,z(u,(s,_)=>(i(),K(g,{model:s.valueModel,label:s.label,key:_},null,8,["model","label"]))),128))]),e("div",w1,[(i(),d(w,null,z(C,(s,_)=>e("div",{key:_,onMousedown:p=>R(s),onMouseup:t[1]||(t[1]=p=>B())},[e("div",{class:Y(s.class)},[r(s1)],2)],40,y1)),64))])])])]),r(N,{"open-dialog":$.value,"where-project":"body"},{default:I(()=>[r(G,{title:"You have to pay",onGenericMsg:t[2]||(t[2]=s=>F(s)),"close-button":!0},{default:I(()=>[e("div",$1,[e("div",z1,[e("div",null,[C1,e("span",I1,M(c.value),1)]),k1]),e("div",x1,[e("div",null,[P1,e("span",S1,M(l.value),1)]),D1]),e("div",R1,[e("div",null,[V1,e("span",B1,M(C[x.value.fuel-1].price),1)]),F1])])]),_:1})]),_:1},8,["open-dialog"])],64))}});const E1=b(L1,[["__scopeId","data-v-a10d0194"]]);export{E1 as default};
