5ed3957fca50af8f23922870fcd6fac7d02f06ae
( Nechiforel David-Samuel ) NsdHSO <nechiforelsamuel@yahoo.com>
2023-07-07 07:32:47 +0300

    feat:🎸 refactor more dinamically

--- src/views/PumpView.vue
+++ src/views/PumpView.vue
@@ -12,47 +12,45 @@ const littersRef = ref(0)
 const presetRef = ref(0)
 const increaseIntervalRef = ref(0)
 const openRef = ref(false)
-const fuelsRef = reactive([
-  {
-    valueModel: 0,
-    label: 'Fuel 1',
-    fuel: 1
-  },
-  {
-    valueModel: 0,
-    label: 'Fuel 2',
-    fuel: 2
-  },
-  {
-    valueModel: 0,
-    label: 'Fuel 3',
-    fuel: 3
-  }
-])
-const whoIsChange = computed(() =>
-  fuelsRef ? fuelsRef.find((s) => s.valueModel > 0) : 0
-)
+
 const disabledPressed = ref(true)
-const pumpMetaData = [
+const pumpMetaData = reactive([
   {
     class: 'red-three',
     name: 'Red',
     fuel: 2,
-    price: 1.2
-  },
-  {
-    class: 'green-one',
-    name: 'Green',
-    fuel: 0,
-    price: 3.2
+    price: 1.2,
+    valueModel: 0,
+    label: 'Fuel 1'
   },
   {
     class: 'blue-two',
     name: 'Blue',
+    fuel: 0,
+    price: 3.1,
+    valueModel: 0,
+    label: 'Fuel 2'
+  },
+  {
+    class: 'green-one',
+    name: 'Green',
     fuel: 1,
-    price: 2.2
+    price: 2.4,
+    valueModel: 0,
+    label: 'Fuel 3'
   }
-] as { class: string; name: string; fuel: number; price: number }[]
+]) as {
+  price: number
+  valueModel: number
+  label: string
+  class: string
+  name: string
+  fuel: number
+  price: number
+}[]
+const whoIsChange = computed(() =>
+  pumpMetaData ? pumpMetaData.find((s) => s.valueModel > 0) : 0
+)
 
 function keyPressed($event) {
   if ($event.item !== 'Clear') {
@@ -69,17 +67,18 @@ function addContAtEveryDelayStep(
   currentFuel: number,
   increment: number,
   fuel,
-  delay: number
+  delay: number,
+  idx
 ) {
   for (let i = 0; i <= steps; i++) {
     setTimeout(() => {
       currentFuel += increment
-      fuelsRef[fuel.fuel].valueModel = +currentFuel.toFixed(3)
+      pumpMetaData[idx].valueModel = +currentFuel.toFixed(3)
     }, i * delay)
   }
 }
 
-function putInCar(fuel) {
+function putInCar(fuel, idx) {
   const increment = fuel.price / 100
   const duration = 300 // Duration in milliseconds
   const steps = 100 // Number of steps for the progress bar
@@ -88,10 +87,10 @@ function putInCar(fuel) {
   dollarRef.value = 0
   littersRef.value = 0
 
-  fuelsRef.forEach((fuel) =>
+  pumpMetaData.forEach((fuel) =>
     fuel.valueModel > 0 ? (fuel.valueModel = 0) : fuel.valueModel
   )
-  addContAtEveryDelayStep(steps, currentFuel, increment, fuel, delay)
+  addContAtEveryDelayStep(steps, currentFuel, increment, fuel, delay, idx)
   increaseLitter(delay)
   watch(dollarRef, (newValueDollar) => {
     littersRef.value = +(newValueDollar / fuel.price).toFixed(3)
@@ -114,7 +113,7 @@ function closeDialogAction(event) {
     openRef.value = false
     dollarRef.value = 0
     littersRef.value = 0
-    fuelsRef.forEach((fuel) =>
+    pumpMetaData.forEach((fuel) =>
       fuel.valueModel > 0 ? (fuel.valueModel = 0) : fuel.valueModel
     )
   }
@@ -143,24 +142,23 @@ function closeDialogAction(event) {
         <ShowValueComponent :model="dollarRef" label="Dollar" />
         <ShowValueComponent :model="littersRef" label="Liters" />
       </div>
-      <div class="pump__bottom__fuel">
-        <div class="pump__bottom__fuel-price">
-          <ShowValueComponent
-            v-for="(item, idx) in fuelsRef"
-            :model="item.valueModel"
-            :label="item.label"
-            :key="idx"
-          />
-        </div>
-        <div class="pump__bottom__fuel-pump">
-          <div
-            v-for="(item, idx) in pumpMetaData"
-            :key="idx"
-            @mousedown="putInCar(item)"
-            @mouseup="clearIntervalIncrease()"
-          >
-            <div :class="item.class">
-              <PumpIcon />
+      <div class="pump-display">
+        <div
+          class="pump__bottom__fuel"
+          v-for="(item, idx) in pumpMetaData"
+          :key="idx"
+        >
+          <div class="pump__bottom__fuel-price">
+            <ShowValueComponent :model="item.valueModel" :label="item.label" />
+          </div>
+          <div class="pump__bottom__fuel-pump">
+            <div
+              @mousedown="putInCar(item, idx)"
+              @mouseup="clearIntervalIncrease()"
+            >
+              <div :class="item.class">
+                <PumpIcon />
+              </div>
             </div>
           </div>
         </div>
@@ -200,7 +198,7 @@ function closeDialogAction(event) {
           <div>
             <span> Price per liter: </span>
             <span class="generic-invoice__price-money">
-              {{ pumpMetaData[whoIsChange.fuel - 1].price }}
+              {{ whoIsChange.price }}
             </span>
           </div>
           <div class="generic-invoice__price-label">
@@ -315,6 +313,7 @@ function closeDialogAction(event) {
       font-family: digital;
       font-size: 2rem;
     }
+
     &-label {
       display: flex;
       justify-content: center;
@@ -322,15 +321,22 @@ function closeDialogAction(event) {
     }
   }
 }
+
 .liter {
   border: 4px solid var(--color-border);
   border-radius: 1rem;
   padding: 0 1rem;
   transition: all 0.5s;
   gap: 0.2rem;
+
   &:hover {
     opacity: 0.9;
     background: var(--color-border-hover);
   }
+}
+
+.pump-display {
+  display: flex;
+  gap: 1rem;
 }
 </style>
